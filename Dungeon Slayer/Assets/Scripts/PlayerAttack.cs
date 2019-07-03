950ad7a56b83ad2551048c24d130c4d847552829
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2019-07-03 16:57:44 -0300

    Simple attacking completed

--- Dungeon Slayer/Assets/Scripts/PlayerAttack.cs
+++ Dungeon Slayer/Assets/Scripts/PlayerAttack.cs
@@ -6,33 +6,37 @@ public class PlayerAttack : MonoBehaviour
 {
     public Transform attack;
     public LayerMask enemyLayer;
+    public GameObject hitEffect;
     public float attackRange;
     public int attackDamage;
     private float attackDelay;
     public float initAttackDelay;
 
-    // Update is called once per frame
+    // Essa funcao e chamada a cada frame
     void Update()
     {
         if (attackDelay <= 0) {
-            // The player can attack
-            if (Input.GetAxisRaw("Attack") == 1) {
+            // O jogador pode atacar
+            if (Input.GetAxisRaw("Attack") == 1) {  // Se o jogador pressionou o botao de ataque
+                // Cria um circulo na posicao de ataque
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attack.position, attackRange, enemyLayer);
-                
+                // Todos os Colliders (inimigos) encontrados sofrem dano
                 foreach (Collider2D enemy in enemiesToDamage) {
                     Debug.Log(enemy);
-                    // Fazer o inimigo sofrer dano
+                    // TODO: o inimigo sofre dano
                 }
 
-                Debug.Log("Attacked!!");
+                // Instancia o efeito de acerto de ataque (e o destroi depois de certo tempo)
+                Destroy(Instantiate(hitEffect, attack.position, Quaternion.identity), 0.4f);   // TODO: colocar dentro do foreach
                 attackDelay = initAttackDelay;
             }
         }
-        else {
+        else {  // Vai decrescendo o tempo de espera para que o jogador possa atacar de novo
             attackDelay -= Time.deltaTime;
         }
     }
 
+    // Essa funcao permite visualizar na "Scene View" a bolinha de colisao
     void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attack.position, attackRange);
