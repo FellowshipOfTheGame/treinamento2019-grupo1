e00a55ae8d44e7ad7f567e97bf96ee09f314e700
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2019-08-12 23:11:38 -0300

    Implemented Dungeon heart and started to make the final countdown

---
+++ Dungeon Slayer/Assets/Scripts/DungeonScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonScript : MonoBehaviour {
+    
+    public Slider healthBar;
+    public Text timeText;
+    public Animator cameraAnimator;
+    [SerializeField] private float health = 10f;
+    [SerializeField] private float timeToEscape = 20f;
+    private float curHealth;
+    private GameObject[] doors;
+    
+    // Essa funcao e chamada antes do primeiro Update
+    void Start() {
+        curHealth = health;
+        // Acha todas as portas da cena
+        doors = GameObject.FindGameObjectsWithTag("Door");
+        // Desativa todas
+        foreach (GameObject obj in doors) obj.SetActive(false);
+    }
+
+    // Esta funcao e chamada a cada frame
+    void Update() {
+        if (curHealth <= 0) {
+            // A dungeon morreu
+            if (healthBar != null) Destroy(healthBar.gameObject, 0.2f); // Desativa sua barra de vida apos um certo tempo
+            timeText.gameObject.SetActive(true);    // Ativa o texto que ira mostrar o tempo restante para sair da Dungeon
+            DontDestroyOnLoad(timeText.transform.parent.gameObject); // Faz com que o texto nao seja destruido ao se trocar de cena
+            DontDestroyOnLoad(gameObject);    // Faz com que este objeto nao seja destruido ao se trocar de cena (para preservar o script8)
+            Destroy(GetComponent<SpriteRenderer>());    // Destroi a sprite do coracao
+            Destroy(GetComponent<CircleCollider2D>());    // Destroi o collider do coracao (ele ira carregar, literalmente, so o script)
+            StartCoroutine(Collapse(timeToEscape));
+            // Abre todas as portas para o jogador
+            foreach (GameObject obj in doors) obj.SetActive(true);
+        }
+        // Atualiza a barra de vida
+        if (healthBar != null) healthBar.SetValueWithoutNotify(curHealth/health);
+    }
+
+    // Funcao que sera chamada sempre que o boss tiver de receber dano
+    public void TakeDamage(int amount) {
+        if (curHealth > 0) curHealth -= amount;
+        cameraAnimator.SetTrigger("Shake"); // Avisa a Camera para tremer
+        // animator.SetTrigger("HasTakenDamage");
+    }
+
+    private IEnumerator Collapse(float initTime) {
+        float curTime = initTime;
+        string minutes, seconds;
+        while (curTime > 0) {
+            minutes = Mathf.Floor(curTime/60).ToString("00");
+            seconds = Mathf.Floor(curTime%60).ToString("00");
+            timeText.text = minutes + ":" + seconds;
+            curTime -= Time.deltaTime;
+            yield return null;
+        }
+        Application.Quit();
+    }
+}
---
+++ Dungeon Slayer/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class PlayerManager : MonoBehaviour {
+    
+    public static PlayerManager instance;
+
+    public PlayerMovement movementScript;
+    public PlayerAttack attackScript;
+    public Animator animator;
+    public SpriteRenderer sprite;
+    public Slider healthBar;
+    [SerializeField] private float health = 30f;
+    private float curHealth;
+
+    // Essa funcao e chamada quando o objeto e instanciado pela primeira vez
+    void Awake() {
+        // So pode existir uma instancia dessa classe
+        if (instance == null) {
+            instance = this;
+            //healthBar.enabled = true; // Achar uma forma de habilitar
+        }
+        else {
+            // O jogador fica na posicao inicial da sua instancia naquela sala
+            PlayerManager.instance.transform.position = this.transform.position;
+            // A outra instancia e destruida
+            Destroy(gameObject);
+            return;
+        }
+        // Este objeto (Player) ira persistir por todas as cenas
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(healthBar.transform.parent.gameObject);
+        // A vida do jogador e colocada
+        curHealth = health;
+    }
+
+    // Essa funcao e chamada antes do primeiro Update
+    void Start() {
+        //CinemachineVirtualCamera vcam = GameObject.FindWithTag("VirtualCamera").GetComponent<CinemachineVirtualCamera>();
+        //vcam.m_Follow = this.transform;
+    }
+
+    // Esta funcao e chamada a cada frame
+    void Update() {
+        if (curHealth <= 0) {
+            // O jogador morreu
+            Destroy(gameObject);    // Destroi o jogador
+            // Chama a cena de Fim de Jogo
+            Application.Quit();
+        }
+        // Atualiza a barra de vida
+        healthBar.SetValueWithoutNotify(curHealth/health);
+    }
+    
+    // Funcao que sera chamada sempre que o jogador tiver de receber dano
+    public void TakeDamage(int amount) {
+        if (curHealth > 0) curHealth -= amount;
+        // animator.SetTrigger("HasTakenDamage");
+    }
+
+    public void SetMovement(bool canPlayerMove) {
+        movementScript.canMove = canPlayerMove;
+    }
+
+    public void SetAttack(bool canPlayerAttack) {
+        attackScript.canAttack = canPlayerAttack;
+    }
+
+    public void SetColor(Color c) {
+        if (sprite != null) sprite.color = c;
+    }
+}
